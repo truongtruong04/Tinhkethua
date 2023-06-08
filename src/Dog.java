78c8da3e754bbab132d6f29feab1939782cb3744
truongkhongsai <anhp31746@gmail.com>
2023-06-09 01:19:22 +0700

    fist commit

---
+++ src/Dog.java
@@ -0,0 +1,8 @@
+public class Dog extends Animal  {
+        public void bark(){
+            System.out.println("chó đái bờ rào  ");
+
+        }
+    }
+
+
---
+++ src/Student.java
@@ -0,0 +1,7 @@
+public class Student {
+    public static void main(String [] args ){
+        Dog myDog = new Dog();
+        myDog.eat();
+        myDog.bark();
+    }
+}
